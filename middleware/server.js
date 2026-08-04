637fbce25076cb9a3256df4e0f4ecd717f399dcf
Suraj00345 <surajkumarmanna2000@gmail.com>
2026-08-04 22:08:27 +0530

    Ecommerce setup

--- middleware/server.js
+++ middleware/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 // const router = require("./routes/index");
 // const bookRouter = require("./routes/books");
-const courses = require('./routes/courses')
-const student = require('./routes/student')
+// const courses = require('./routes/courses')
+// const student = require('./routes/student')
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+
 const app = express();
 const PORT = 3000;
 
@@ -10,8 +14,14 @@ app.use(express.json());
 
 // app.use("/",router);
 // app.use("/",bookRouter);
-app.use("/students",student)
-app.use("/courses",courses)
+
+// app.use("/students",student)
+// app.use("/courses",courses)
+
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
+
 
 app.use((req,res)=>{
     res.status(404).send("Route not found");
