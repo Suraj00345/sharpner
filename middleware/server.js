10ecbdb49b74598ad47b454a332589ae6475efb3
Suraj00345 <surajkumarmanna2000@gmail.com>
2026-08-04 20:56:51 +0530

    Book router

--- middleware/server.js
+++ middleware/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const router = require("./routes/index");
+const bookRouter = require("./routes/books");
 const app = express();
 const PORT = 3000;
-const router = require("./routes/index");
 
 app.use(express.json());
 
-app.use("/",router);
+// app.use("/",router);
+app.use("/",bookRouter);
 
 app.use((req,res)=>{
     res.status(404).send("Route not found");
